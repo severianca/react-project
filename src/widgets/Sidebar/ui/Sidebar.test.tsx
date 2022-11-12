d1b0747d711328d87506cff7873e2dc0b3fe4b29
severianca <malub4@yandex.ru>
2022-11-12 16:34:02 +0500

    fix test

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, screen } from '@testing-library/react'
 import Sidebar from 'widgets/Sidebar/ui/Sidebar'
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation'
+import { componentRender } from 'shared/lib/tests/componentRender'
 
 describe('Sidebar', () => {
 	test('проверка создания sidebar', () => {
-		renderWithTranslation(<Sidebar />)
+		componentRender(<Sidebar />)
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument()
 	})
 })
 
 describe('Sidebar', () => {
 	test('проверка toggle', () => {
-		renderWithTranslation(<Sidebar />)
+		componentRender(<Sidebar />)
 		const toggleBtn = screen.getByTestId('sidebar-toggle') // получить кнопку по ее data-testid
 		fireEvent.click(toggleBtn) // нажатие на кнопку
 		expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
